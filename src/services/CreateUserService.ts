591f2f789ed933967d40cc5e41af4fed17afb330
Luiz Alexandre Silva <alexandrehsilva96@gmail.com>
2020-04-21 16:04:33 -0400

    create users

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('E-mail address in use.');
+    }
+
+    const hashedPasword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPasword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
