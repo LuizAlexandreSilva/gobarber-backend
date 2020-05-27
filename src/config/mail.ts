5f912c7afeacca559b9b538a7de6283536643a9b
Luiz Alexandre Silva <alexandrehsilva96@gmail.com>
2020-05-27 19:56:05 -0400

    emails from Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'alexandrehsilva96@gmail.com',
+      name: 'Alexandre Silva',
+    },
+  },
+} as IMailConfig;
